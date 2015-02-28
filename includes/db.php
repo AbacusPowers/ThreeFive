1ec90619a508207b8a9be8503d6906d53a407f2d
AbacusPowers <justin@360zen.com>
2015-02-28 12:35:45 -0600

    Delete db.php

--- includes/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-try {
-    $handler = new PDO('mysql:host=127.0.0.1;dbname=threefive','root','UnPreDic7able');
-    $handler->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $ex) {
-    echo $ex->getMessage();
-}
